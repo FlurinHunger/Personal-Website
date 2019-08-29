59ed16dce8d394d12d4bc37c97f2e2b8f9920b80
GeneTv <lpgenehd@gmail.com>
2019-08-29 21:18:08 +0200

    .

--- public/main.js
+++ public/main.js
@@ -1,6 +1,3 @@
-$.get("https://flurin.ml/component/head.html", function(data) {
-  $("head").append(data);
-});
 $.get("https://flurin.ml/component/header.html", function(data) {
   $("header").append(data);
 });
