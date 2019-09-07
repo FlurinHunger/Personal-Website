efb8f5c047e7cc13f6ba379c56a9596d7d3eaa7d
fuckmeplz <47223054+fuckmeplz@users.noreply.github.com>
2019-09-07 21:14:56 +0200

    f

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,8 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+exports.getIP = functions.https.onRequest((request, response) => {
+  response.status(200).json({
+    userIP: request.headers['x-forwarded-for'] || request.connection.remoteAddress
+  });
+});
