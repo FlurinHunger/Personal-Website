049201214b436f945e3151120840698ae371d30b
fuckmeplz <47223054+fuckmeplz@users.noreply.github.com>
2019-09-08 16:29:30 +0200

    Update

--- public/index.js
+++ public/index.js
@@ -7,3 +7,103 @@ var typed = new Typed(".cd-words-wrapper", {
   loop: true,
   smartBackspace: true
 });
+$(function() {
+  "use strict";
+
+  var wind = $(window);
+  // scrollIt
+  $.scrollIt({
+    upKey: 38,                // key code to navigate to the next section
+    downKey: 40,              // key code to navigate to the previous section
+    easing: 'swing',          // the easing function for animation
+    scrollTime: 600,          // how long (in ms) the animation takes
+    activeClass: 'active',    // class given to the active nav element
+    onPageChange: null,       // function(pageIndex) that is called when page is changed
+    topOffset: -63            // offste (in px) for fixed top navigation
+  });
+
+
+  // Change navbar background on scroll
+  wind.on("scroll",function () {
+    var bodyScroll = wind.scrollTop(),
+    navbar = $(".navbar");
+    if(bodyScroll > 100){
+      navbar.addClass("nav-scroll");
+    } else{
+      navbar.removeClass("nav-scroll");
+    }
+  });
+
+
+
+    // progress bar
+  /*  wind.on('scroll', function () {
+        $(".skills-progress span").each(function () {
+            var bottom_of_object =
+            $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window =
+            $(window).scrollTop() + $(window).height();
+            var myVal = $(this).attr('data-value');
+            if(bottom_of_window > bottom_of_object) {
+                $(this).css({
+                  width : myVal
+                });
+            }
+        });
+    }); */
+});
+
+
+// === window When Loading === //
+
+$(window).on("load",function (){
+
+    var wind = $(window);
+
+    // Preloader
+    $(".loading").fadeOut(500);
+
+
+    // stellar
+    wind.stellar();
+
+
+    // isotope
+    $('.gallery').isotope({
+      // options
+      itemSelector: '.items'
+    });
+
+    var $gallery = $('.gallery').isotope({
+      // options
+    });
+
+    // filter items on button click
+    $('.filtering').on( 'click', 'span', function() {
+
+        var filterValue = $(this).attr('data-filter');
+
+        $gallery.isotope({ filter: filterValue });
+
+    });
+
+    $('.filtering').on( 'click', 'span', function() {
+
+        $(this).addClass('active').siblings().removeClass('active');
+
+    });
+});
+
+
+function loadEvents() {
+        var mailString;
+        function updateMailString() {
+            mailString = '?subject=' + encodeURIComponent($('#subject').val())
+                + '&body=' + encodeURIComponent($('#message').val());
+            $('#mail-link').attr('href',  'mailto:person@email.com' + mailString);
+        }
+        $( "#subject" ).focusout(function() { updateMailString(); });
+        $( "#message" ).focusout(function() { updateMailString(); });
+        updateMailString();
+    }
+    loadEvents();
