316ab54a8b09a6c604d37b2f922bf69c56d4ae92
fuckmeplz <47223054+fuckmeplz@users.noreply.github.com>
2019-09-20 21:49:59 +0200

    jj

--- public/index.js
+++ public/index.js
@@ -75,13 +75,3 @@ function loadEvents() {
   updateMailString();
 }
 loadEvents();
-
-
-$(document).ready(function(){
-    setTimeout(function () {
-        $("#cookieConsent").fadeIn(200);
-     }, 4000);
-    $("#closeCookieConsent, .cookieConsentOK").click(function() {
-        $("#cookieConsent").fadeOut(200);
-    });
-}); 
